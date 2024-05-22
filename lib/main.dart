4b8dd6fe2a8e100778730b51170cab1acfa9d14f
notosgnikusnsi <notosgnikusnsi@gmail.com>
2024-05-22 16:45:12 +0900

    feat: 現在向いている方角を角度で取得し表示するウィジェットを作成する

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_compass/flutter_compass.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,8 +15,48 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Compass(),
         ),
+      ),
+    );
+  }
+}
+
+class Compass extends StatefulWidget {
+  const Compass({super.key});
+
+  @override
+  State<Compass> createState() => _CompassState();
+}
+
+class _CompassState extends State<Compass> {
+  double? _direction;
+  late StreamSubscription<CompassEvent> _compassSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    _compassSubscription = FlutterCompass.events!.listen((CompassEvent event) {
+      setState(() {
+        _direction = event.heading;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _compassSubscription.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Compass Example'),
+      ),
+      body: Center(
+        child: Text('Current Direction: ${_direction?.toStringAsFixed(2)}°'),
       ),
     );
   }
